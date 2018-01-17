7a9f5c40ce034ca50537e1130042781202fcdeb9
Luke <luke@luke.co.pl>
2018-01-17 21:46:33 +0100

    New website

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source "https://rubygems.org"
-
-gem 'jekyll'
-gem 'github-pages', '109', group: :jekyll_plugins
-gem 'jekyll-github-metadata'
---
+++ generate-index.js
@@ -0,0 +1,26 @@
+let fs = require('fs'),
+    prismjs = require('prismjs');
+
+let info = fs.readFileSync('info.json', 'utf8'),
+    infoHtml = prismjs.highlight(info, prismjs.languages.javascript),
+    css = fs.readFileSync('node_modules/prismjs/themes/prism-tomorrow.css', 'utf8');
+
+infoHtml = infoHtml.replace(/https?:\/\/[^\s/$.?#].[^\s"]*/ig, match => `<a class="token url" href="${match}">${match}</a>`);
+
+css += '\n\n' + 'pre { overflow: visible !important; } html { background: #2d2d2d; }';
+
+html = `<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Jakub Młokosiewicz – hckr.pl</title>
+<style>
+${css}
+</style>
+</head>
+<body>
+<pre class="language-js">${infoHtml}</pre>
+</body>
+</html>`;
+
+fs.writeFileSync('index.html', html, 'utf8');
