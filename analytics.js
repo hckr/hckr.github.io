ff00eaaa83e74d41c5ede1276fe5725a003deef1
Luke <luke@luke.co.pl>
2020-04-07 04:05:29 +0200

    Move analytics code from topbar.js to analytics.js, force text-align: left in topbar

---
+++ analytics.js
@@ -0,0 +1,13 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'UA-162891460-1', {
+    'page_path': location.pathname
+});
+
+(function (d) {
+    var gt = d.createElement('script'), s = d.scripts[0];
+    gt.src = 'https://www.googletagmanager.com/gtag/js?id=UA-162891460-1';
+    gt.async = true;
+    s.parentNode.insertBefore(gt, s);
+})(document);
--- topbar.js
+++ topbar.js
@@ -1,17 +1,3 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'UA-162891460-1', {
-    'page_path': location.pathname
-});
-
-(function (d) {
-    var gt = d.createElement('script'), s = d.scripts[0];
-    gt.src = 'https://www.googletagmanager.com/gtag/js?id=UA-162891460-1';
-    gt.async = true;
-    s.parentNode.insertBefore(gt, s);
-})(document);
-
 window.WebFontConfig = {
     google: {
         families: ['Open Sans']
@@ -62,7 +48,7 @@ document.body.insertAdjacentHTML('afterbegin', `
     }
 }
 </style>
-<div style="position: absolute; top: 0; left: 0; right: 0; padding-left: 10px; background: #25262b;">
+<div style="position: absolute; top: 0; left: 0; right: 0; padding-left: 10px; background: #25262b; text-align: left;">
 <a class="linkbackhckrpl" href="https://hckr.pl" style="font-weight: bold; text-transform: lowercase; color: #8cc4f9; display: inline-block; height: 30px; text-decoration: none;">
 <span style="font-size: 18px">See my other projects</span> <span class="url" style="font-size: 16px; color: #ccc;">// go to hckr.pl</span>
 </a>
