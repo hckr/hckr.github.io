478af6049f166743a1e396c91c7988fbc6490d89
Luke <luke@luke.co.pl>
2020-04-07 04:32:55 +0200

    Fix for topbar.js: filler was too small on mobile

--- topbar.js
+++ topbar.js
@@ -13,7 +13,6 @@ window.WebFontConfig = {
 
 
 document.body.insertAdjacentHTML('afterbegin', `
-<div style="height: 30px"></div>
 <style>
 @keyframes blink {
     from {
@@ -40,14 +39,23 @@ document.body.insertAdjacentHTML('afterbegin', `
     animation: 3s infinite blink;
 }
 
+.linkbackhckrplfiller {
+    height: 30px;
+}
+
 @media screen and (max-width: 600px) {
     .url {
         display: block;
         position: relative;
         top: -5px;
     }
+
+    .linkbackhckrplfiller {
+        height: 50px;
+    }
 }
 </style>
+<div class="linkbackhckrplfiller"></div>
 <div style="position: absolute; top: 0; left: 0; right: 0; padding-left: 10px; background: #25262b; text-align: left;">
 <a class="linkbackhckrpl" href="https://hckr.pl" style="font-weight: bold; text-transform: lowercase; color: #8cc4f9; display: inline-block; height: 30px; text-decoration: none;">
 <span style="font-size: 18px">See my other projects</span> <span class="url" style="font-size: 16px; color: #ccc;">// go to hckr.pl</span>
