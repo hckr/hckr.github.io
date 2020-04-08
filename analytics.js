d16e8b08cea0833f1ceaae000cd884c211454012
Luke <luke@luke.co.pl>
2020-04-08 03:18:51 +0200

    Add cookie information

--- analytics.js
+++ analytics.js
@@ -1,3 +1,19 @@
+if (!document.cookie.split(';').map(c => c.trim()).includes('cookieAlertDismissed=1')) {
+    document.body.insertAdjacentHTML('beforeend', `
+        <div style="position: sticky; bottom: 0; left: 0; right: 0; z-index: 1000; padding: 5px 10px; background: #25262b; color: #ccc; text-align: left; font-size: 16px;">
+            This site uses cookies for statistical purposes.
+            You can deny cookies from any domain in your browser settings.
+            <span
+                style="padding: 3px; cursor:pointer; text-decoration: underline; border: none;"
+                title="Accept and close this message"
+                onclick="document.cookie='cookieAlertDismissed=1; expires=Tue, 19 Jan 2038';this.parentNode.parentNode.removeChild(this.parentNode);"
+            >
+                close ✖
+            </span>
+        </div>
+    `);
+}
+
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
