20bc61b47ecce3f65ca8ad2a52dc97cdce1a6897
Luke <luke@luke.co.pl>
2020-07-05 01:43:17 +0200

    Add .prettierrc, add link to Google privacy policy

--- analytics.js
+++ analytics.js
@@ -1,22 +1,27 @@
-if (navigator.cookieEnabled !== false && !document.cookie.split(';').map(c => c.trim()).includes('cookieAlertDismissed=1')) {
+if (
+    navigator.cookieEnabled !== false &&
+    !document.cookie
+        .split(';')
+        .map(c => c.trim())
+        .includes('cookieAlertDismissed=1')
+) {
     let message, closeText;
     switch (document.documentElement.lang) {
         case 'pl':
             message = `
-            Ta strona wykorzystuje pliki cookie do celów statystycznych (przesyła dane do Google Analytics).
-            W ustawieniach przeglądarki można zablokować odpowiednie pliki cookie.
-            `;
+            Ta strona wykorzystuje pliki cookie do celów statystycznych (Google Analytics, <a style="text-decoration: underline; color: inherit" href="https://www.google.com/policies/privacy/partners/">polityka prywatności</a>).
+            W ustawieniach przeglądarki można zablokować odpowiednie pliki cookie.`;
             closeText = 'zamknij';
             break;
         default:
             message = `
-            This website uses cookies for statistical purposes (transmits data to Google Analytics).
-            You can block the relevant cookies in your browser settings.
-            `;
+            This website uses cookies for statistical purposes (Google Analytics, <a style="text-decoration: underline; color: inherit" href="https://www.google.com/policies/privacy/partners/">privacy policy</a>).
+            You can block the relevant cookies in your browser settings.`;
             closeText = 'close';
     }
-    document.body.insertAdjacentHTML('beforeend', `
-        <div style="position: sticky; bottom: 0; left: 0; right: 0; z-index: 1000; padding: 5px 10px; background: #25262b; color: #ccc; text-align: left; font-size: 16px; margin-top: 10px;">
+    document.body.insertAdjacentHTML(
+        'beforeend',
+        `<div style="position: sticky; bottom: 0; left: 0; right: 0; z-index: 1000; padding: 5px 10px; background: #25262b; color: #ccc; text-align: left; font-size: 16px; margin-top: 10px;">
             ${message}
             <span
                 style="padding: 3px; cursor:pointer; text-decoration: underline; border: none;"
@@ -25,19 +30,22 @@ if (navigator.cookieEnabled !== false && !document.cookie.split(';').map(c => c.
             >
                 ${closeText} ✖
             </span>
-        </div>
-    `);
+        </div>`
+    );
 }
 
 window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
+function gtag() {
+    dataLayer.push(arguments);
+}
 gtag('js', new Date());
 gtag('config', 'UA-162891460-1', {
-    'page_path': location.pathname
+    page_path: location.pathname,
 });
 
 (function (d) {
-    var gt = d.createElement('script'), s = d.scripts[0];
+    var gt = d.createElement('script'),
+        s = d.scripts[0];
     gt.src = 'https://www.googletagmanager.com/gtag/js?id=UA-162891460-1';
     gt.async = true;
     s.parentNode.insertBefore(gt, s);
